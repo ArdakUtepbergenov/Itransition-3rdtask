f4b4f9e80a2bc6dc7c3db63b6c1c18156395bea1
ArdakUtepbergenov <utepbergenovardak8@gmail.com>
2026-06-11 18:20:06 +0500

    Initial commit

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,15 +6,20 @@ int lcm = 0;
 
 
 
-app.MapGet("/utepbergenovardak8_gmail_com", (int x, int y) =>
+app.MapGet("/utepbergenovardak8_gmail_com", (string x, string y) =>
 {
 
-    bool areNatural = x > 0 && y > 0;
+    if (!(int.TryParse(x, out int xNum) && int.TryParse(y, out int yNum)))
+    {
+    return "NaN";
+    }
+
+    bool areNatural = xNum > 0 && yNum > 0;
     if (areNatural)
     {
-        for (int i = Math.Max(x,y); i <= x*y; i++)
+        for (int i = Math.Max(xNum,yNum); i <= xNum*yNum; i++)
     {
-        if (i % x == 0 && i % y == 0)
+        if (i % xNum == 0 && i % yNum == 0)
         {
             lcm = i;
             break;
