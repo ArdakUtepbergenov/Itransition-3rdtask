5dd2d6241cd43167bbe047f696934511c9085ddc
ArdakUtepbergenov <utepbergenovardak8@gmail.com>
2026-06-11 18:48:44 +0500

    Initial commit

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,14 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-int lcm = 0;
+
 
 
 
 
 app.MapGet("/utepbergenovardak8_gmail_com", (string x, string y) =>
 {
-
+    int lcm = 0;
     if (!(int.TryParse(x, out int xNum) && int.TryParse(y, out int yNum)))
     {
     return "NaN";
